2c8c91f74d636871e4bc317c6c2fef28bcd28bb3
Sergey Braumann <sergeybraumann@gmail.com>
2026-01-08 15:41:23 +0500

    revert: change AVAILABLE_COMMANDS back to available_commands_update
    Per ACP SDK v0.12 type definitions, the correct session update type is 'available_commands_update'

--- src/config/constants.js
+++ src/config/constants.js
@@ -97,7 +97,7 @@ export const SESSION_UPDATE = {
   TOOL_CALL: "tool_call",
   TOOL_CALL_UPDATE: "tool_call_update",
   PLAN: "plan",
-  AVAILABLE_COMMANDS: "available_commands",
+  AVAILABLE_COMMANDS: "available_commands_update",
   CURRENT_MODE: "current_mode_update",
   CURRENT_MODEL: "current_model_update",
 };
